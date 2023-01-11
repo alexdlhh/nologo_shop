c45b9a0746714a2086c965d281464c3f916251cb
Alex <alexdlhh@gmail.com>
2023-01-11 22:27:59 +0100

    carrousel changes

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -34,11 +34,11 @@
 @else
 <div class="row" id="profile">
     <div class="col s1">
-        <div class="vertical-text">            
-            <div class="areapersonal">Área Personal de<br> {{Auth::user()->name}}</div>
+        <div class="vertical-text">  
             <div class="selector-vertical">
                 <img src="/rfeg_ico_arrow_white.svg" width="18" height="18" alt="">
-            </div> 
+            </div>           
+            <div class="areapersonal">Área Personal de<br> {{Auth::user()->name}}</div>            
         </div>
     </div>
     <div class="col s11 area_personal_fix">
