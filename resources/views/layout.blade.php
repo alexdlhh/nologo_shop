8dd51dfee9cc30b49360a3fb66693799664cd83a
Alex <alexdlhh@gmail.com>
2023-01-11 20:21:10 +0100

    carrousel changes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -260,28 +260,28 @@
                             <div class="col s6">
                                 <h7 class="white-text">RFEG</h7>
                                 <ul>
-                                    <li><a class="grey-text" href="/rfeg/comunicados">Presentación</a></li>
-                                    <li><a class="grey-text" href="/rfeg/rfeg">Quiénes Somos</a></li>                                                    
-                                    <li><a class="grey-text" href="/rfeg/gobierno">Organo de gobierno</a></li>                                                    
-                                    <li><a class="grey-text" href="/rfeg/normativa/reglamentos">Normativa</a></li>                                                    
-                                    <li><a class="grey-text" href="/rfeg/transparencia">Ley de Transparencia</a></li>                                                    
-                                    <li><a class="grey-text" href="/rfeg/estatutos">Estatutos</a></li>                                                    
-                                    <li><a class="grey-text" href="/rfeg/elecciones">Elecciones</a></li>                                                    
+                                    <li><a class="azul_rfeg" href="/rfeg/comunicados">Presentación</a></li>
+                                    <li><a class="azul_rfeg" href="/rfeg/rfeg">Quiénes Somos</a></li>                                                    
+                                    <li><a class="azul_rfeg" href="/rfeg/gobierno">Organo de gobierno</a></li>                                                    
+                                    <li><a class="azul_rfeg" href="/rfeg/normativa/reglamentos">Normativa</a></li>                                                    
+                                    <li><a class="azul_rfeg" href="/rfeg/transparencia">Ley de Transparencia</a></li>                                                    
+                                    <li><a class="azul_rfeg" href="/rfeg/estatutos">Estatutos</a></li>                                                    
+                                    <li><a class="azul_rfeg" href="/rfeg/elecciones">Elecciones</a></li>                                                    
                                 </ul>
                                 <h7 class="white-text">ESPECIALIDADES</h7>
                                 <ul>
-                                    <li><a class="grey-text" href="/especialidades/artistica-masculina/">Artística masculina</a></li>
-                                    <li><a class="grey-text" href="/especialidades/artistica-femenina/">Artística femenina</a></li>
-                                    <li><a class="grey-text" href="/especialidades/ritmica/">Ritmica</a></li>
-                                    <li><a class="grey-text" href="/especialidades/trampolin/">Trampolin</a></li>
-                                    <li><a class="grey-text" href="/especialidades/aerobica/">Aeróbica</a></li>                                                    
-                                    <li><a class="grey-text" href="/especialidades/para-todos/">Para Todos</a></li>                                                    
-                                    <li><a class="grey-text" href="/especialidades/parkour/">Parkour</a></li>                                                    
+                                    <li><a class="azul_rfeg" href="/especialidades/artistica-masculina/">Artística masculina</a></li>
+                                    <li><a class="azul_rfeg" href="/especialidades/artistica-femenina/">Artística femenina</a></li>
+                                    <li><a class="azul_rfeg" href="/especialidades/ritmica/">Ritmica</a></li>
+                                    <li><a class="azul_rfeg" href="/especialidades/trampolin/">Trampolin</a></li>
+                                    <li><a class="azul_rfeg" href="/especialidades/aerobica/">Aeróbica</a></li>                                                    
+                                    <li><a class="azul_rfeg" href="/especialidades/para-todos/">Para Todos</a></li>                                                    
+                                    <li><a class="azul_rfeg" href="/especialidades/parkour/">Parkour</a></li>                                                    
                                 </ul>
                                 <h7 class="white-text">MULTIMEDIA</h7>
                                 <ul>
-                                    <li><a class="grey-text" href="/media">Fotos y vídeos</a></li>
-                                    <li><a class="grey-text" href="/revistas">Revistas</a></li>                                                   
+                                    <li><a class="azul_rfeg" href="/media">Fotos y vídeos</a></li>
+                                    <li><a class="azul_rfeg" href="/revistas">Revistas</a></li>                                                   
                                 </ul>
                             </div>
                             <div class="col s6">
@@ -289,21 +289,21 @@
                                 <div class="br"> </div>
                                 <h7 class="white-text">CALENDARIO</h7>
                                 <ul>
-                                    <li><a class="grey-text" href="/calendar/ritmica/nacional">Competiciones Nacionales</a></li>
-                                    <li><a class="grey-text" href="/calendar/ritmica/internacional">Competiciones Internacionales</a></li>                                                    
+                                    <li><a class="azul_rfeg" href="/calendar/ritmica/nacional">Competiciones Nacionales</a></li>
+                                    <li><a class="azul_rfeg" href="/calendar/ritmica/internacional">Competiciones Internacionales</a></li>                                                    
                                 </ul>
                                 <h7 class="white-text">ESCUELA NACIONAL</h7>
                                 <ul>
-                                    <li><a class="grey-text" href="/schools">Cursos</a></li>
-                                    <li><a class="grey-text" href="/normativa">Normativa</a></li>                                                    
+                                    <li><a class="azul_rfeg" href="/schools">Cursos</a></li>
+                                    <li><a class="azul_rfeg" href="/normativa">Normativa</a></li>                                                    
                                 </ul>
                                 <h7 class="white-text">INFORMACION LEGAL</h7>
                                 <ul>
-                                    <li><a class="grey-text" href="/contacto">Contacto</a></li>
-                                    <li><a class="grey-text" href="/pagina/legal">Aviso Legal</a></li>
-                                    <li><a class="grey-text" href="/pagina/cookies">Política de Cookies</a></li>
-                                    <li><a class="grey-text" href="/pagina/privacidad">Política de Privacidad</a></li>
-                                    <li><a class="grey-text" href="/mapa">Mapa</a></li>                                                    
+                                    <li><a class="azul_rfeg" href="/contacto">Contacto</a></li>
+                                    <li><a class="azul_rfeg" href="/pagina/legal">Aviso Legal</a></li>
+                                    <li><a class="azul_rfeg" href="/pagina/cookies">Política de Cookies</a></li>
+                                    <li><a class="azul_rfeg" href="/pagina/privacidad">Política de Privacidad</a></li>
+                                    <li><a class="azul_rfeg" href="/mapa">Mapa</a></li>                                                    
                                 </ul>
                             </div>
                         </div>
@@ -328,7 +328,7 @@
                 <div class="footer-copyright">
                     <div class="container">
                     © 2022 Copyright Nologo
-                    <a class="grey-text text-lighten-4 right" href="nologo.es">Nologo.es</a>
+                    <a class="azul_rfeg text-lighten-4 right" href="nologo.es">Nologo.es</a>
                 </div>
             </div>
         </footer>
